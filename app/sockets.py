e4e6d57c991e3fd4e18fb16c8c94b5977319abff
Rais Iqbal <bahshat@gmail.com>
2025-07-14 22:27:44 +0530

    Channel logic added

--- app/sockets.py
+++ app/sockets.py
@@ -3,6 +3,10 @@ from app.extensions import socketio
 from app.services.system_monitor import get_current_metrics
 from app.services.historic_data import get_historic_metrics
 
+# Define room names
+CURRENT_METRICS_ROOM = 'current_metrics_room'
+HISTORIC_DATA_ROOM = 'historic_data_room'
+
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
@@ -13,34 +17,43 @@ def handle_disconnect():
 
 @socketio.on('join')
 def on_join(data):
-    room = data['room']
-    join_room(room)
-    emit('status', {'msg': 'Joined room ' + room}, room=room)
+    device_type = data.get('device_type', 'web')  # Default to 'web'
+    
+    join_room(CURRENT_METRICS_ROOM)
+    emit('status', {'msg': f'Joined {CURRENT_METRICS_ROOM}'})
+
+    if device_type == 'mobile':
+        join_room(HISTORIC_DATA_ROOM)
+        emit('status', {'msg': f'Joined {HISTORIC_DATA_ROOM}'})
+
 
 @socketio.on('leave')
 def on_leave(data):
-    room = data['room']
-    leave_room(room)
-    emit('status', {'msg': 'Left room ' + room}, room=room)
+    device_type = data.get('device_type', 'web')
+    
+    leave_room(CURRENT_METRICS_ROOM)
+    emit('status', {'msg': f'Left {CURRENT_METRICS_ROOM}'})
+    
+    if device_type == 'mobile':
+        leave_room(HISTORIC_DATA_ROOM)
+        emit('status', {'msg': f'Left {HISTORIC_DATA_ROOM}'})
+
 
 @socketio.on('get_current_metrics')
-def handle_get_current_metrics(data):
-    room = data.get('room')
-    if room:
-        metrics = get_current_metrics()
-        emit('current_metrics_response', metrics, room=room)
+def handle_get_current_metrics():
+    metrics = get_current_metrics()
+    emit('current_metrics_response', metrics, room=CURRENT_METRICS_ROOM)
 
 @socketio.on('get_historic_metrics')
 def handle_get_historic_metrics(data):
-    room = data.get('room')
     start_time = data.get('start_time')
     end_time = data.get('end_time')
 
-    if room and start_time and end_time:
+    if start_time and end_time:
         def background_task():
             for chunk in get_historic_metrics(start_time, end_time):
-                emit('historic_data_chunk', chunk, room=room)
-                socketio.sleep(0.1) # Yield to other tasks
-            emit('historic_data_finished', room=room)
+                emit('historic_data_chunk', chunk, room=HISTORIC_DATA_ROOM)
+                socketio.sleep(0.1)  # Yield to other tasks
+            emit('historic_data_finished', room=HISTORIC_DATA_ROOM)
 
         socketio.start_background_task(background_task)
