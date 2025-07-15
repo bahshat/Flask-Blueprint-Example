586e30af67d091d74457529deae03383dccb52ba
Rais Iqbal <bahshat@gmail.com>
2025-07-16 01:12:13 +0530

    Removed historic room

--- app/sockets.py
+++ app/sockets.py
@@ -1,12 +1,9 @@
 from flask_socketio import emit, join_room, leave_room
 from app.extensions import socketio
-from app.services.historic_data import get_historic_metrics
 from app.services import system_monitor
 
 # Define room names
 CURRENT_METRICS_ROOM = 'current_metrics_room'
-HISTORIC_DATA_ROOM = 'historic_data_room'
-
 
 def send_current_metrics(metrics):
     socketio.emit('current_metrics_response', metrics, room=CURRENT_METRICS_ROOM)
@@ -18,42 +15,19 @@ system_monitor.register_metrics_callback(send_current_metrics)
 def handle_connect():
     print('Client connected')
 
+
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
+
 @socketio.on('join')
-def on_join(data):
-    device_type = data.get('device_type', 'web')  # Default to 'web'
-    
+def on_join(data):    
     join_room(CURRENT_METRICS_ROOM)
     emit('status', {'msg': f'Joined {CURRENT_METRICS_ROOM}'}, room=CURRENT_METRICS_ROOM)
 
-    if device_type == 'mobile':
-        join_room(HISTORIC_DATA_ROOM)
-        emit('status', {'msg': f'Joined {HISTORIC_DATA_ROOM}'}, room=HISTORIC_DATA_ROOM)
 
 @socketio.on('leave')
-def on_leave(data):
-    device_type = data.get('device_type', 'web')
-    
+def on_leave(data):    
     leave_room(CURRENT_METRICS_ROOM)
     emit('status', {'msg': f'Left {CURRENT_METRICS_ROOM}'}, room=CURRENT_METRICS_ROOM)
-    
-    if device_type == 'mobile':
-        leave_room(HISTORIC_DATA_ROOM)
-        emit('status', {'msg': f'Left {HISTORIC_DATA_ROOM}'}, room=HISTORIC_DATA_ROOM)
-
-@socketio.on('get_historic_metrics')
-def handle_get_historic_metrics(data):
-    start_time = data.get('start_time')
-    end_time = data.get('end_time')
-
-    if start_time and end_time:
-        def background_task():
-            for chunk in get_historic_metrics(start_time, end_time):
-                emit('historic_data_chunk', chunk, room=HISTORIC_DATA_ROOM)
-                socketio.sleep(0.1) 
-            emit('historic_data_finished', room=HISTORIC_DATA_ROOM)
-
-        socketio.start_background_task(background_task)
